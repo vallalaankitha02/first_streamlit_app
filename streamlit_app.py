df9ab5742673d516bde35c4c45da58b5c8533290
vallalaankitha02 <130159490+vallalaankitha02@users.noreply.github.com>
2023-04-13 18:51:44 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,11 +26,11 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
+
+#take the json version of the rsponse and normaliz it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+#output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
 
